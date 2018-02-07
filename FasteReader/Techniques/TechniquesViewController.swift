4f8bd59b5b874c04a2821143ff02d65de342ebc1
Goran Pavlovic <goranpavlovic@Gorans-MacBook-Pro.local>
2018-02-07 11:38:47 +0100

    Finished toolbar UI on Techniques View

--- FasteReader/Techniques/TechniquesViewController.swift
+++ FasteReader/Techniques/TechniquesViewController.swift
@@ -8,13 +8,66 @@
 
 import UIKit
 
+
+
+enum ToolbarIdentifier: Int{
+    case wordFlash = 0
+    case diamond = 1
+    case textHighlight = 2
+    case extendVision = 3
+}
+
+// MARK: Properties and Initialization
+
 class TechniquesViewController: UIViewController {
+
+    
+    @IBOutlet var toolbarButtons: [UIButton]!
+    
+    var previousPressedButton: UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-
 }
+// MARK: Toolbar
+extension TechniquesViewController{
+    
+    @IBAction func toolBarButtonPressed(_ sender: UIButton){
+        
+        
+        setToolbarColor(color: .cyan)
+        sender.backgroundColor = UIColor.white
+        
+        //sender.backgroundColor = UIColor.black
+        
+        switch sender.tag {
+        case ToolbarIdentifier.wordFlash.rawValue:
+            print("Word flash")
+        case ToolbarIdentifier.diamond.rawValue:
+            print("Diamond")
+        case ToolbarIdentifier.textHighlight.rawValue:
+            print("Text highlight")
+        case ToolbarIdentifier.extendVision.rawValue:
+            print("Extend vision")
+        default:
+            print("Error another choice happened that was not registered")
+        }
+    }
+    
+    func setToolbarColor(color: UIColor){
+        for button in toolbarButtons{
+            button.backgroundColor = color
+        }
+    }
+    
+}
+
+
+
+
+
+
+
