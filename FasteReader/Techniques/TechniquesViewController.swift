7b2681d62f55a00becb66c173d908533f6d8a133
Goran Pavlovic <goranpavlovic@Gorans-MacBook-Pro.local>
2018-02-07 09:36:51 +0100

    Created TechniquesViewController

---
+++ FasteReader/Techniques/TechniquesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TechniquesViewController.swift
+//  FasteReader
+//
+//  Created by Goran Pavlovic on 2/7/18.
+//  Copyright © 2018 Goran Pavlovic. All rights reserved.
+//
+
+import UIKit
+
+class TechniquesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
