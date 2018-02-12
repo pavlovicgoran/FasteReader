5603eac857766d92a17a2058360b27f0398b6690
Goran Pavlovic <goranpavlovic@Gorans-MacBook-Pro.local>
2018-02-12 11:31:42 +0100

    ProfileView GUI finished added Editi UIBarButton

--- FasteReader/Profile/ProfileViewController.swift
+++ FasteReader/Profile/ProfileViewController.swift
@@ -13,21 +13,55 @@ class ProfileViewController: UIViewController {
 
     @IBOutlet weak var currentlyReading: UIButton!
     
+    @IBOutlet weak var lineLength: UILabel!
+    @IBOutlet weak var numberOfLines: UILabel!
+    @IBOutlet weak var wordsPerMinute: UILabel!
+    @IBOutlet weak var username: UILabel!
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
         currentlyReading.disclosureButton(baseColor: view.tintColor)
+        updateReadingMaterial()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(changeUsername))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(pickPhoto))
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
         updateReadingMaterial()
     }
     
+    //Reading material button
     @IBAction func chooseReadingMaterial(_ sender: UIButton) {
         chooseReadingMaterial()
     }
     
+    //Line length stepper
+    @IBAction func lineLengthChanged(_ sender: UIStepper) {
+        lineLength.text = "Line Length: \(Int(sender.value))"
+    }
+    
+    //Number of lines stepper
+    @IBAction func numberOfLinesChanged(_ sender: UIStepper) {
+        numberOfLines.text = "Number of Lines: \(Int(sender.value))"
+    }
+    
+    //Words per minute slider
+    @IBAction func wordsPerMinuteChanged(_ sender: UISlider) {
+        wordsPerMinute.text = "\(Int(sender.value))"
+    }
+    
+    
 }
 
-//MARK: Currently reading button
+// MARK: Currently reading button
 extension ProfileViewController: LibraryPicker{
     func chooseReadingMaterial(){
         let tabBar = tabBarController as! BookTabController
@@ -40,3 +74,38 @@ extension ProfileViewController: LibraryPicker{
         currentlyReading.setTitle(BookChapter.currentlyReading, for: .normal)
     }
 }
+
+// MARK: Navbar buttons
+extension ProfileViewController{
+    
+    @objc func changeUsername(){
+        print("changeUsername")
+        let ac = UIAlertController(title: "Enter your username", message: nil, preferredStyle: .alert)
+        ac.addTextField(configurationHandler:{ (_ textField: UITextField) -> Void in
+            textField.placeholder = "Enter username"
+            
+        })
+        ac.addAction(UIAlertAction(title: "Save", style: .default, handler: { [unowned self](_ action: UIAlertAction) in
+            
+            if (!(ac.textFields![0].text?.isEmpty)!){
+                self.username.text = ac.textFields![0].text
+            }
+        }))
+        
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(ac, animated: true)
+    }
+    
+    @objc func pickPhoto(){
+        print("pickPhoto")
+    }
+    
+}
+
+
+
+
+
+
