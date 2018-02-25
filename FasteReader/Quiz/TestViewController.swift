c0c5cefa7ed7a53998f61f48e719854e8d459ab0
Goran Pavlovic <goranpavlovic@Gorans-MacBook-Pro.local>
2018-02-25 21:38:05 +0100

    Added test View UI

---
+++ FasteReader/Quiz/TestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TestViewController.swift
+//  FasteReader
+//
+//  Created by Goran Pavlovic on 2/25/18.
+//  Copyright © 2018 Goran Pavlovic. All rights reserved.
+//
+
+import UIKit
+// MARK: Properties and Initialization
+class TestViewController: UIViewController {
+
+    @IBOutlet weak var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stylizingStartButton()
+    }
+
+    private func stylizingStartButton(){
+        startButton.layer.borderWidth = 2
+        startButton.layer.borderColor = UIColor(red: 255/255, green: 221/255, blue: 129/255, alpha: 1).cgColor
+        startButton.layer.cornerRadius = 8
+        
+    }
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        //startButton.layer.cornerRadius = 16
+    }
+}
